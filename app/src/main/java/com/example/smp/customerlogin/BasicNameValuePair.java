763b7571692e6af112c6002a1619cb86323e4a27
Dipak <dipakbelsare@gmail.com>
2017-06-30 19:10:30 +0530

    Logo hide from login Layout

---
+++ app/src/main/java/com/example/smp/customerlogin/BasicNameValuePair.java
@@ -0,0 +1,10 @@
+package com.example.smp.customerlogin;
+
+/**
+ * Created by Sunil on 29-Jun-2017.
+ */
+public class BasicNameValuePair extends NameValuePair {
+    public BasicNameValuePair(String name, String value) {
+        super();
+    }
+}
---
+++ app/src/main/java/com/example/smp/customerlogin/NameValuePair.java
@@ -0,0 +1,7 @@
+package com.example.smp.customerlogin;
+
+/**
+ * Created by Sunil on 29-Jun-2017.
+ */
+public class NameValuePair {
+}
